e8e273af5cc0e944e159adc4bdc1e46c0645deaa
Sideeqkolade <sideeqkolade@gmail.com>
2023-08-02 01:08:17 +0100

    task 0 and 1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+export default function getListStudentIds(arrayOfObjects) {
+  // check if the input is an array
+  if (!Array.isArray(arrayOfObjects)) {
+    return []
+  } 
+  
+  // extract student id from each object
+  return arrayOfObjects.map((student) => student.id);
+}
