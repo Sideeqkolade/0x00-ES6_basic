97de226076705fe55abc30c677dc8da8a5c1493b
Sideeqkolade <sideeqkolade@gmail.com>
2023-09-19 17:08:05 +0100

    task 0 completed

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,7 @@
-#!/usr/bin/node
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ */
 
 const displayMessage = (message) => {
   process.stdout.write(`${message} \n`);
